02a2ced4534dc48dcf44eb17ad765f8c660b54b3
Lee de Mora <ledm@pml.ac.uk>
2020-05-08 15:37:30 +0100

    Added extra data to the job list

--- shelve_to_csv.py
+++ shelve_to_csv.py
@@ -57,6 +57,57 @@ def save_as_csv(field, jobID, units, label, data):
         'u-bf647': 'Historical',
         'u-bf656': 'Historical',
         'u-bf703': 'Historical',
+
+        # R1
+        'u-bc179': 'UKESM1 Historical run (2250)',
+        'u-be509': 'UKESM1 SSP1 2.6 R1 (2250)',
+        'u-be537': 'UKESM1 SSP2 4.5 R1 (2250)',
+        'u-be647': 'UKESM1 SSP3 7.0 R1 (2250)',
+        'u-be653': 'UKESM1 SSP5 8.5 R1 (2250)',
+        'u-bh409': 'UKESM1 SSP1 1.9 R1 (2250)',
+        'u-bh454': 'UKESM1 SSP4 3.4 R1 (2250)',
+        'u-bh456': 'UKESM1 SSP5 3.4-OS R1 (2250)',
+
+        #R2
+        'u-bc292': 'UKESM1 Historical run (2165)',
+        'u-be606': 'UKESM1 SSP1 2.6 R2 (2165)',
+        'u-be679': 'UKESM1 SSP2 4.5 R2 (2165)',
+        'u-be690': 'UKESM1 SSP3 7.0 R2 (2165)',
+        'u-be693': 'UKESM1 SSP5 8.5 R2 (2165)',
+        'u-bh570': 'UKESM1 SSP1 1.9 R2 (2165)',
+        'u-bh712': 'UKESM1 SSP4 3.4 R2 (2165)',
+        'u-bh744': 'UKESM1 SSP5 3.4-OS R2 (2165)',
+
+        #R3
+        'u-bc370': 'UKESM1 Historical run (2120)',
+        'u-be682': 'UKESM1 SSP1 2.6 R3 (2120)',
+        'u-be683': 'UKESM1 SSP2 4.5 R2 (2120)',
+        'u-be684': 'UKESM1 SSP3 7.0 R3 (2120)',
+        'u-be686': 'UKESM1 SSP5 8.5 R3 (2120)',
+        'u-bh716': 'UKESM1 SSP1 1.9 R3 (2120)',
+        'u-bh717': 'UKESM1 SSP4 3.4 R3 (2120)',
+        'u-bh718': 'UKESM1 SSP5 3.4-OS R3 (2120)',
+
+        #R4
+        'u-bb075': 'UKESM1 Historical run (1960) with new SO2 emissions height',
+        'u-be335': 'UKESM1 SSP3 7.0 R4 (1960)',
+        'u-be392': 'UKESM1 SSP5 8.5 R4 (1960)',
+        'u-be393': 'UKESM1 SSP1 2.6 R4 (1960)',
+        'u-be394': 'UKESM1 SSP2 4.5 R4 (1960)',
+        'u-bh210': 'UKESM1 SSP1 1.9 R4 (1960)',
+        'u-bh254': 'UKESM1 SSP4 3.4 R4 (1960)',
+        'u-bh285': 'UKESM1 SSP5 3.4-OS R4 (1960)',
+
+        #R8
+        'u-bb277': 'UKESM1 Historical run (2560), with new SO2 emissions height',
+        'u-be395': 'UKESM1 SSP3 7.0 R8 (2560)',
+        'u-be396': 'UKESM1 SSP5 8.5 R8 (2560)',
+        'u-be397': 'UKESM1 SSP1 2.6 R8 (2560)',
+        'u-be398': 'UKESM1 SSP2 4.5 R8 (2560)',
+        'u-bh807': 'UKESM1 SSP1 1.9 R8 (2560)',
+        'u-bh808': 'UKESM1 SSP4 3.4 R8 (2560)',
+        'u-bh809': 'UKESM1 SSP5 3.4-OS R8 (2560)',
+            
     	}
 
 
@@ -88,9 +139,11 @@ def get_path(jobID, field):
     return path
 
 
+all_jobs = ['u-aw310', 'u-az513', 'u-az515', 'u-az524', 'u-bb075', 'u-bb277', 'u-bc179', 'u-bc292', 'u-bc370', 'u-bc470', 'u-bd288', 'u-bd416', 'u-bd483', 'u-be335', 'u-be392', 'u-be393', 'u-be394', 'u-be395', 'u-be396', 'u-be397', 'u-be398', 'u-be509', 'u-be537', 'u-be606', 'u-be647', 'u-be653', 'u-be679', 'u-be682', 'u-be683', 'u-be684', 'u-be686', 'u-be690', 'u-be693', 'u-bf647', 'u-bf656', 'u-bf703', 'u-bh210', 'u-bh254', 'u-bh285', 'u-bh409', 'u-bh454', 'u-bh456', 'u-bh570', 'u-bh717', 'u-bh718', 'u-bh744', 'u-bh807', 'u-bh808', 'u-bh809']
 
 def main():
-    jobs = ['u-aw310', ]
+    jobs = all_jobs
+
     fields = {'TotalIntegratedPrimaryProduction': ('regionless', 'layerless', 'metricless'), }
     units  = {'TotalIntegratedPrimaryProduction': 'Gt/yr', }
     labels = {'TotalIntegratedPrimaryProduction': 'Total Integrated Primary Production', }
@@ -99,4 +152,6 @@ def main():
             path = get_path(jobID, field)
             data = load_shelve(path, data_key)
             save_as_csv(field, jobID, units[field], labels[field], data)
-main()
+
+if __name__ == "__main__":
+    main()
